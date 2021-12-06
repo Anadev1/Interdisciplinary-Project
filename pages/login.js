3bebe4181ccfb2df1310e9b254ea50a5e2c79ec0
Ana Iacovache <eaaania@students.eaaa.dk>
2021-12-06 23:10:01 +0100

    login page

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,5 @@
 import loader from "../components/loader.js";
+import nav from "../components/nav.js";
 import router from "../router.js";
 import service from "../service.js";
 
@@ -17,13 +18,23 @@ export default class LoginPage {
       "beforeend",
       /*html*/ `
             <section id="${this.id}" class="page">
-              <h1>${this.id}</h1>
+                <div class="login__logo-container">
+                  <img src="../img/shareat-logo.svg" alt="logo" class="login__logo" />
+                </div>
+                <div class="login__form-container">
+                  <form class="login__form">
+                    <input id="login-email" type="text" name="email" placeholder="Email" required>
+                    <input id="login-password" type="password" placeholder="Password" autocomplete="new-password" required>
+                    <a class="login__button btn__large">Login</a>
+                  </form>
+                </div>
+                <p class="login__signup-text">Don't have an account? <a href="" class="login__signup-link">Sign up</a></p>
             </section>
         `
     );
   }
 
-  beforeShow(props) {
-    console.log(props);
+  beforeShow() {
+    nav.hide();
   }
 }
