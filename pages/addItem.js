94a2e7b9727a1c910b964ff27ab17da398395304
Ana Iacovache <eaaania@students.eaaa.dk>
2021-12-09 10:35:29 +0100

    add item page style

--- pages/addItem.js
+++ pages/addItem.js
@@ -13,7 +13,24 @@ export default class AddItemPage {
   render() {
     this.domElement.innerHTML += /*html*/ `
             <section id="additem" class="page">
-              <h1>additem</h1>
+              <div class="additem__title-container">
+                <h2 class="additem__title">ADD ITEM</h2>
+              </div>
+
+              <div class="additem__form-container">
+               <form class="additem__form">
+                <input type="text" name="name" id="name" placeholder="Item Name" /><br>
+                <input type="textarea" name="description" id="description" placeholder="Item Description" /><Br>
+                <input type="file" name="images" id="images" accept="image/png, image/jpg" />
+               </form>
+              </div>
+
+              <div class="additem__allergens-container">
+                <h2 class="additem__allergens-title">Allergens/Dietary restrictions</h2>
+                <div class="additem__search-container">
+                  <input type="search" name="search" id="search" placeholder="Search..." />
+                </div>
+              </div>
             </section>
         `;
   }
