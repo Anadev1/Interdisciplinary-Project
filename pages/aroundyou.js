217bdf565541fc14b05b55f096b1cfe3db9f16e8
Ana Iacovache <eaaania@students.eaaa.dk>
2021-12-09 01:00:27 +0100

    style done

--- pages/aroundyou.js
+++ pages/aroundyou.js
@@ -13,7 +13,50 @@ export default class AroundYouPage {
   render() {
     this.domElement.innerHTML += /*html*/ `
             <section id="aroundyou" class="page">
-              <h1>aroundyou</h1>
+              <div class="aroundyou__title-container">
+                <h2 class="aroundyou__title">FEED</h2>
+              </div>
+
+              <h2 class="aroundyou__subtitle">Around you</h2>
+              
+              <div class="aroundyou__items-container">
+                <div class="aroundyou__item-container">
+                  <img src="../img/eggs.png" alt="item" class="aroundyou__item-image" />
+                  <p class="aroundyou__item-details">kr. 10 - Egg carton</p>
+                  <span class="aroundyou__item-expiration">EXP: 2 days</span>
+                </div>
+
+                <div class="aroundyou__item-container">
+                  <img src="../img/candy.png" alt="item" class="aroundyou__item-image" />
+                  <p class="aroundyou__item-details">kr. 5 - Various treats</p>
+                  <span class="aroundyou__item-expiration">EXP: 45 days</span>
+                </div>
+
+                <div class="aroundyou__item-container">
+                  <img src="../img/meat.png" alt="item" class="aroundyou__item-image" />
+                  <p class="aroundyou__item-details">kr. 20 - Beef steak</p>
+                  <span class="aroundyou__item-expiration">EXP: 3 days</span>
+                </div>
+
+                <div class="aroundyou__item-container">
+                  <img src="../img/salad.png" alt="item" class="aroundyou__item-image" />
+                  <p class="aroundyou__item-details">kr. 16 - Veggies mix</p>
+                  <span class="aroundyou__item-expiration">EXP: 4 days</span>
+                </div>
+
+                <div class="aroundyou__item-container">
+                  <img src="../img/banana.png" alt="item" class="aroundyou__item-image" />
+                  <p class="aroundyou__item-details"><span>FREE</span> - Banana</p>
+                  <span class="aroundyou__item-expiration">EXP: ???</span>
+                </div>
+
+                <div class="aroundyou__item-container">
+                  <img src="../img/avocado.png" alt="item" class="aroundyou__item-image" />
+                  <p class="aroundyou__item-details">kr. 5 - Avocado</p>
+                  <span class="aroundyou__item-expiration">EXP: ???</span>
+                </div>
+              </div>
+
             </section>
         `;
   }
