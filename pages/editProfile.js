98fd71c3dd94072fc249304b9675bd1926531aae
Ana Iacovache <eaaania@students.eaaa.dk>
2021-12-09 11:36:35 +0100

    edit profile page

--- pages/editProfile.js
+++ pages/editProfile.js
@@ -13,7 +13,26 @@ export default class EditProfilePage {
   render() {
     this.domElement.innerHTML += /*html*/ `
             <section id="editprofile" class="page">
-              <h1>editprofile</h1>
+              <div class="editprofile__title-container">
+                <h2 class="editprofile__title">EDIT PROFILE</h2>
+                <a class="btn btn__small editprofile__save-button">SAVE</a>
+              </div>
+
+              <div class="editprofile__info-container">
+                <img src="../img/profile.png" class="editprofile__image"/>
+                <form class="editprofile__form">
+                  <input type="text" name="first-name" id="first-name" placeholder="Maria"/>
+                  <input type="text" name="last-name" id="last-name" placeholder="Thompson"/>
+                  <input type="text" placeholder="mthompson@gmail.com"/>
+                </form>
+              </div>
+
+              <div class="editprofile__preferences-container">
+                <h2 class="editprofile__preferences-title">Preferences</h2>
+                <div class="editprofile__search-container">
+                  <input type="search" name="search" id="search" placeholder="Search and add" />
+                </div>
+              </div>
             </section>
         `;
   }
